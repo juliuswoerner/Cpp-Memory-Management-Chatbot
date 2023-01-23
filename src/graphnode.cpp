938c7ef208810c47c65d30d60f14349a99eff502
juliuswoerner <j14wq73@gmail.com>
2023-01-23 15:36:16 +0100

    aseg

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -40,8 +40,8 @@ void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
 ////
 void GraphNode::MoveChatbotHere(ChatBot chatbot)
 {
-    _chatBot = std::move(chatbot);
-    _chatBot.SetCurrentNode(this);
+    _chatBot = *chatbot;
+    _chatBot->SetCurrentNode(this);
 }
 
 void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
